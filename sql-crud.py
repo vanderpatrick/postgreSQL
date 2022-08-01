a0e734abc4472f73f286a78584e8d86a1dcf99a5
vanderpatrick <patrick123321123@icloud.com>
2022-08-01 14:12:15 +0000

    Finalize crud functionality

--- sql-crud.py
+++ sql-crud.py
@@ -96,16 +96,59 @@ patrick_alexander = Programmer(
 )
 
 #  add each instance of our programmers to our session
-# session.add(ada_lovelace)
-# session.add(alan_turing)
-# session.add(grace_hopper)
-# session.add(margaret_hamilton)
-# session.add(bill_gates)
-# session.add(tim_berners_lee)
-# session.add(patrick_alexander)
+session.add(ada_lovelace)
+session.add(alan_turing)
+session.add(grace_hopper)
+session.add(margaret_hamilton)
+session.add(bill_gates)
+session.add(tim_berners_lee)
+session.add(patrick_alexander)
 
 #  Commit our session to the database
 session.commit()
+
+
+# updating a single record 
+# programmer = session.query(Programmer).filter_by(id=12).first()
+# programmer.famous_for = "World President"
+
+
+# Updating multiples records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("gender not defined")
+#     session.commit()
+# # Commit our session to the database
+# session.commit()
+
+
+# deleting a single record
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# # defensive programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
+
+# delete multiple records
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     session.delete(programmer)
+#     session.commit()
 
 #  query the database to find all programers
 programmers = session.query(Programmer)
